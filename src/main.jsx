f19adca5f73d72adeea101b19d328436daa62064
benjaaminh <benjamin.heikkala@abo.fi>
2023-12-29 15:56:57 +0200

    change to hashrouter from browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter as Router} from 'react-router-dom'
-
+import {HashRouter as Router} from 'react-router-dom'
+//use hashrouter to fix refresh problem in github pages
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
